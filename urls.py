df2c41844ff33920911f138f187240f22822174b
alrubio5 <79871252+alrubio5@users.noreply.github.com>
2021-03-01 14:04:20 -0600

    Delete urls.py

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from . import views
-
-app_name = "dashboard"
-urlpatterns = [
-    path('', views.index, {'pagename': ''}, name='home'),
-    path('<str:pagename>', views.index, name='index'),
-]
